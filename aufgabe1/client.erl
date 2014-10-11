1b33c062ee18ca05d48e1d2d8ea94bf8a54c44a1
Jan Lepel <janlepel@ws-76-178.haw-1x.haw-hamburg.de>
2014-10-11 14:24:44 +0200

    added some new functions in client.erl and added the config files

--- aufgabe1/client.erl
+++ aufgabe1/client.erl
@@ -1,9 +1,44 @@
--module(client).
--export([send/1]).
+- module(client).
+- import(werkzeug, [to_String/1, timeMilliSecond/0, get_config_value/2]).
+%- export([start/2, send/2, message_builder/0, dropmessage/3]).
+- compile(export_all).
 
-send(Server) -> 
+
+start(Hostname, Adress) ->
+  {ok, ConfigListe} = file:consult("client.cfg"),
+  {ok, Servername} = get_config_value(servername, ConfigListe),
+  PID = get_PID(Servername, Hostname, Adress).
+  %Connect = spawn(fun() -> starting()),
+  %register(client, Connect).
+
+
+get_PID(Servername, Hostname, Adress) ->
+	{Servername, list_to_atom(lists:concat([Hostname, "@", Adress]))}.
+
+
+send(Server, Msg) -> 
 	Server ! {getmessages, self()},
 	receive
 		{reply, Number, Nachricht, Terminated} ->
 			Nachricht	
 	end.
+
+
+ping_server(Hostname, Adress) ->
+	net_adm:ping(erlang:list_to_atom(lists:concat([Hostname,"@", Adress]))).
+
+
+%TODO: Hier fehlt noch die Anzahl der Nachrichten
+message_builder() ->
+	Massage = lists:concat([to_String(node()), "-", to_String(self()), "-C-1-01:", "Sendezeit: ", timeMilliSecond(), "|\n"]).
+
+
+dropmessage(Server, Message, Number) ->
+	Server ! {dropmessage, {Number, Message}}.
+
+
+
+
+ starting() ->
+ 	message_builder().
+
