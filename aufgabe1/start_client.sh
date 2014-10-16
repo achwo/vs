eb1826e98a5a7f605c8f04ab76714057798d6b55
Felix Jensen <felixjensn@gmail.com>
2014-10-16 16:33:18 +0200

    added shell scripts for starting server and client (not functional, only descriptions)

---
+++ aufgabe1/start_client.sh
@@ -0,0 +1,6 @@
+# erl -name edgar@141.22.70.220 -setcookie banane
+
+## c(werkzeug).
+## c(client).
+
+## client:start("bla@141.22.90.254").
---
+++ aufgabe1/start_server.sh
@@ -0,0 +1,6 @@
+# erl -name bla@141.22.90.254 -setcookie banane
+
+## c(werkzeug).
+## c(server).
+
+## S = server:start().
