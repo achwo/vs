c80432e9e7dd444ac2105b4c49538ba4f1bffb3c
Felix Jensen <felixjensn@gmail.com>
2014-10-20 16:01:06 +0200

    added client logic for redakteur

--- aufgabe1/client.erl
+++ aufgabe1/client.erl
@@ -12,43 +12,61 @@ start(Hostadress) ->
 
   %TODO: CLIENTNR in LOG-Name ergänzen
   Logfile = lists:concat(["client_", to_String(node()), ".log"]),
-  logging(Logfile, to_String(PID)),
+  Startlog = lists:concat([name(), " Start: ", timeMilliSecond(),"."]),
+  logging(Logfile, Startlog),
+  
+  
   {ok, LifeTime} = get_config_value(lifetime, ConfigListe),
-  timer:kill_after(2 * 1000),
-  loop().
+  OwnMessages = [],
+  %timer:kill_after(LifeTime * 1000 / 45), % das 45 muss weg, ist nur wegen der warning
+  timer:kill_after(3000),
+  %redakteur(1, PID, OwnMessages, Logfile),
+  loop(PID, OwnMessages, Logfile).
 
   %Uid = get_unique_id(PID),
   %io:fwrite ("Uid ~p~n", [Uid]),
   %Message_text = message_builder(Uid, Logfile),
-  %dropmessage(Servername, Message_text, Uid).
+  %dropmessage(Servername, Message_text, Uid). 
   %TODO: wie komme ich am besten an die Number? Bekomme ich so die MSG?
  % dropmessage(PID, Msg, Number).
 
-loop() ->
-  redakteur(5),
-  leser(false),
-  loop().
+name() -> lists:concat([to_String(node()), to_String(self())]).
 
-redakteur(0) -> 
-  io:fwrite("keine pommes!");
-redakteur(HowOften) when HowOften > 0 ->
-  io:fwrite("pommes "),
+
+loop(PID, OwnMessages, Logfile) ->
+  redakteur(5, PID, OwnMessages, Logfile),
+  leser(false, OwnMessages),
+  loop(PID, OwnMessages, Logfile).
+
+redakteur(0, PID, _, Logfile) ->
+  % vergesse, nachricht zu senden 
+  Number = get_unique_id(PID),
+  logging(Logfile, lists:concat([Number, "te Nachricht um ", timeMilliSecond(), " vergessen zu senden ******"]));
+
+redakteur(HowOften, PID, OwnMessages, Logfile) when HowOften > 0 ->
   % warte n sekunden
+  timer:sleep(500),
   % hole nachrichtennummer
+  Number = get_unique_id(PID),
   % adde nummer zur liste selbstgeschickter nachrichten
+  OwnMessagesNew = lists:append(OwnMessages, [Number]),
   % generiere nachricht
+  Message = message_builder(Number),
+  dropmessage(PID, Message, Number),
+  logging(Logfile, Message),
   % sende nachricht
-  redakteur(HowOften-1).
+  redakteur(HowOften-1, PID, OwnMessagesNew, Logfile).
 
-leser(Terminated) when Terminated == true ->
+leser(Terminated, _) when Terminated == true ->
   nix;
-leser(Terminated) when Terminated == false -> 
+
+leser(Terminated, OwnMessages) when Terminated == false -> 
   % hole nachricht
   TerminatedFlag = true, % nur, damit es nicht endlos laeuft im moment :)
   % pruefe, ob nachricht selbstgeschickt
   % generiere ausgabe
   % ausgeben
-  leser(TerminatedFlag).
+  leser(TerminatedFlag, OwnMessages).
 
 receive_message(HowOften) ->
   receive_message(HowOften-1).
@@ -60,12 +78,9 @@ ping_server(Hostname, Adress) ->
 	net_adm:ping(erlang:list_to_atom(lists:concat([Hostname,"@", Adress]))).
 
 
-%TODO: Hier fehlt noch die Anzahl der Nachrichten
-message_builder(Message, Logfile) ->
-	Msg = lists:concat([to_String(node()), "-", to_String(self()), "-C-1-01:", Message,"te Nachricht. Sendezeit: ", timeMilliSecond(), "(", Message, ")\n"]),
-	logging(Logfile, Msg),
-	Msg.
-
+message_builder(MessageNumber) ->
+	lists:concat([to_String(node()), "-", to_String(self()), "-C-1-01:", MessageNumber,"te Nachricht. Sendezeit: ", timeMilliSecond(), "(", MessageNumber, ")\n"]).
+	
 dropmessage(Server, Message, Number) ->
 	Server ! {dropmessage, {Message, Number}}.
 
