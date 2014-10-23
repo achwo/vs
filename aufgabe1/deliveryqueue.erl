f143c0565515861fa5d92bfc7ae9639ad738f229
Johnstar Rainbow <jonny.niebuhr+github@gmail.com>
2014-10-23 13:11:30 +0200

    can compile / get is modified / failer translation in Nachrichtendienst

--- aufgabe1/deliveryqueue.erl
+++ aufgabe1/deliveryqueue.erl
@@ -1,32 +1,27 @@
 -module(deliveryqueue).
 -export([add/3, get/2, get_max_number/0]).
--import(werkzeug, [findSL/2]).
+-import(werkzeug, [findSL/2,findneSL/2]).
 
 add(Content, ID, Queue) ->
 	%TODO: is_full
-	case is_full(Queue)
+	case is_full(Queue) of
 
 		true -> 
 			%TODO: fun for find the lowest ID
-			lists:delete({_,Lowest_ID}, Queue);
+		%	lists:delete({_,Lowest_ID}, Queue),
 			lists:append(Queue, [{Content,ID}]);
 	
 		false ->
-			 lists:append(Queue, [{Content,ID}]);
-	end 
+			 lists:append(Queue, [{Content,ID}])
+	end. 
 
-get(ID, Queue) ->
+get(ID, Queue) -> 
+	TerminatedFlag = ID >= get_max_number(),
+	{findneSL(Queue,ID),TerminatedFlag}.
+		
 
-	%Message = {Content, ID}	
-	Message = findSL(Queue,ID);
+get_max_number() -> todo.
 
-	case Message == {-1,nok} 
-		true -> 
-			[Message, true];
-		false -> 
-			[Message, false];
-	end
-
-
+is_full(Queue) -> todo.
 
 
