d9d2affb9418ea22f9e6f8ea7813f9dc0667b3a1
Jan Lepel <janlepel@ws-78-223.haw-1x.haw-hamburg.de>
2014-10-25 13:08:38 +0200

    added get functions to dlq.erl

--- aufgabe1/dlq.erl
+++ aufgabe1/dlq.erl
@@ -2,10 +2,11 @@
 -import(werkzeug, [findneSL/2]).
 -export([createNew/0, add/3, get/2, getLastMsgNr/1]).
 
-% TODO nachrichtenformat: [{Nachricht, Nr}]
+%nachrichtenformat: [{Nachricht, NewNumber}]
 
 createNew() -> [].
 
+% speichert die Nachtricht in die dql
 add(Message, Number, Queue) ->
 	case is_full(Queue) of 
 		true 	-> NewQueue = lists:append(delete_lowest_id(Queue), [{Message, Number}]);
@@ -14,16 +15,21 @@ add(Message, Number, Queue) ->
 	lists:keysort(2, NewQueue).
 
 get(_, []) -> false;
-get(Number, Queue) ->
-	% TODO suche nach {_, Number}
-	% TODO wenn in get keine nachricht mit der angegebenen nummer gefunden wird, wird "false" zurueckgegeben
-	Message = fake,
-	Number = -1,
-	TerminatedFlag = false,
-	{Message, Number, TerminatedFlag}.
+
+
+get(Number, [{_, NewNumber}|Rest]) when NewNumber /= Number ->
+	get(Number, Rest);
+
+get(_, [{Message, NewNumber}|Rest]) when Rest /= [] ->
+	{Message, NewNumber, true};
+
+get(_, [{Message, NewNumber}|_]) ->
+	{Message, NewNumber, false}.
+
+
 
 getLastMsgNr(Queue) -> 
-	todo.
+	lists:last(Queue).
 
 is_full(Queue) -> 
 	{_, DLQ_max_size} = application:get_env(server, dlq_max_size),
