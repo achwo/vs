697a65a8797e9642108ec384140ef61d488f1c42
Felix Jensen <felixjensn@gmail.com>
2014-10-30 12:51:06 +0100

    implemented server_timer (untested)

--- aufgabe1/server_timer.erl
+++ aufgabe1/server_timer.erl
@@ -1,16 +1,11 @@
 -module(server_timer).
--export([start/1]).
+-export([start/2]).
 
-start(Server) ->
-  %todo content....
-  % every n ms: ping_server(Server)
-  ping_server(Server),
-todo.
+start(Server, WaitingTimeInMS) ->
+  Server ! {ping},
 
-ping_server(Server) ->
-Server ! {ping},
-receive
-  {answer} -> Result = true;
-  {timeout} -> Result = false
-end,
-Result.
+  receive
+    {ping} -> start(Server, WaitingTimeInMS)
+  after WaitingTimeInMS ->
+    Server ! {shutdown}
+  end.
