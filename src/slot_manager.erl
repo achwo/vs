a2014a7eb6b80aec553a989375cb6ae901853051
Felix Jensen <felixjensn@gmail.com>
2015-01-12 16:10:53 +0100

    added reserveSlot and FreeSlotList

--- src/slot_manager.erl
+++ src/slot_manager.erl
@@ -1,34 +1,44 @@
 -module(slot_manager).
 -export([start/0]).
 
-start() -> spawn(fun() -> init() end).
+-define(NUMBER_SLOTS, 25).
 
-init() ->
-  % set sender and receiver
-  % reset FreeSlotList
+start(SyncManager) -> spawn(fun() -> init(SyncManager, nil, nil) end).
+
+init(SyncManager, Sender, Receiver) when Sender /= nil, Receiver /= nil ->
   % start SlotTimer
-  loop().
+  loop(SyncManager, Sender, Receiver, resetFreeSlotList());
+init(SyncManager, Sender, Receiver) ->
+  receive
+    {set_sender, SenderPID} -> 
+      NewSender = SenderPID,
+      init(SyncManager, NewSender, Receiver);
+    {set_receiver, ReceiverPID} ->
+      NewReceiver = ReceiverPID,
+      init(SyncManager, Sender, NewReceiver)
+  end. 
 
 
-loop() ->
+loop(SyncManager, Sender, Receiver, FreeSlotList) ->
   receive 
     {reserve_slot, SlotNumber} -> 
-      reserveSlot(SlotNumber),
-      loop();
+      NewFreeSlotList = reserveSlot(SlotNumber, FreeSlotList),
+      loop(SyncManager, Sender, Receiver, NewFreeSlotList);
     {From, reserve_slot} ->
       reserveRandomSlot(From),
-      loop();
+      loop(SyncManager, Sender, Receiver, FreeSlotList);
     {slot_end} ->
       slotEnd(),
-      loop()
+      loop(SyncManager, Sender, Receiver, FreeSlotList)
   end.
 
 
-reserveSlot(SlotNumber) ->
+reserveSlot(SlotNumber, FreeSlotList) ->
   % remove SlotNumber from FreeSlotList (if possible)
-  todo.
+  lists:delete(SlotNumber, FreeSlotList).
 
 reserveRandomSlot(From) -> 
+  
   % get random Slot from FreeSlotList and return
   % From ! {reserved_slot, SlotNumber}
   todo.
@@ -51,12 +61,14 @@ handleFrameEnd() ->
 
   % transmission_slot setzen
   % sender ! {new_timer, WaitTimeTillTransmissionSlot}
-  % reset FreeSlotList
+  resetFreeSlotList(),
   todo.
 
+resetFreeSlotList() ->
+  createFreeSlotList([], 0, ?NUMBER_SLOTS).
 
-
-
-%{reserve_slot, SlotNumber} -> void: SlotChooser wird über bereits reservierten Slot im nächsten Frame informiert.
-%{reserve_slot} -> {reserved_slot, SlotNumber}: Reserviert einen beliebigen Slot.
-%{slot_end} -> void: Der SlotManager sendet sich via Timer eine Nachricht, sobald ein Slotende erreicht ist.
+createFreeSlotList(List, CurrentSlotNumber, TotalNumber) 
+  when CurrentSlotNumber >= TotalNumber ->
+  List;
+createFreeSlotList(List, CurrentSlotNumber, TotalNumber) ->
+  createFreeSlotList([CurrentSlotNumber | List], CurrentSlotNumber + 1, TotalNumber)
