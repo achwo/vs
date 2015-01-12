fed39a79cecebaefd0136ceef9b2e7ceaac031c5
Felix Jensen <felixjensn@gmail.com>
2015-01-12 15:44:00 +0100

    implemented sync_manager.sync

--- src/sync_manager.erl
+++ src/sync_manager.erl
@@ -1,20 +1,23 @@
 -module(sync_manager).
 -export([start/2]).
 
-start(StationType, TimeOffset) -> 
-  spawn(fun() -> loop(StationType, TimeOffset, []) end).
+start(StationType, TimeOffset) ->  % do we need to know our own station type?
+  spawn(fun() -> loop(TimeOffset, []) end).
 
-loop(StationType, TimeOffset, Deviations) ->
+loop(TimeOffset, Deviations) ->
   receive 
     {add_deviation, StationType, SendTime, ReceiveTime} ->
       NewDeviations = addDeviation(StationType, SendTime, ReceiveTime, Deviations),
-      loop(StationType, TimeOffset, NewDeviations);
+      loop(TimeOffset, NewDeviations);
     {reset_deviations} ->
       NewDeviations = resetDeviations(),
-      loop(StationType, TimeOffset, NewDeviations);
+      loop(TimeOffset, NewDeviations);
     {From, get_current_time} ->
       getCurrentTime(From, TimeOffset),
-      loop(StationType, TimeOffset, Deviations)
+      loop(TimeOffset, Deviations);
+    {sync} ->
+      NewTimeOffset = sync(TimeOffset, Deviations),
+      loop(NewTimeOffset, Deviations)
   end.
 
 addDeviation(StationType, SendTime, ReceiveTime, Deviations) 
@@ -34,3 +37,13 @@ getCurrentTime(From, TimeOffset) ->
 currentTime(TimeOffset) ->
   {MegaSecs, Secs, MicroSecs} = erlang:now(),
   (MegaSecs * 1000000000 + Secs * 1000 + MicroSecs div 1000) + TimeOffset.
+
+sync(TimeOffset, Deviations) when length(Deviations) == 0 ->
+  TimeOffset;
+sync(TimeOffset, Deviations) ->
+  TimeOffset + calculateDeviation(Deviations).
+
+calculateDeviation(Deviations) ->
+  Sum = lists:sum(Deviations),
+  round(Sum / length(Deviations)). %Special case if own station is class A?
+
