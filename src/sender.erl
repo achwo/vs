6fb1b871fb9eac19ee241471b949269382b5a723
Felix Jensen <felixjensn@gmail.com>
2015-01-12 14:53:56 +0100

    sender outline

--- src/sender.erl
+++ src/sender.erl
@@ -5,6 +5,27 @@ start() -> spawn(fun() -> loop() end).
 
 loop() ->
   receive 
-    _ -> io:fwrite("Unknown message")
+    {data, Data} -> 
+      data(Data),
+      loop();
+    {new_timer, WaitTime} -> 
+      newTimer(WaitTime),
+      loop();
+    {send} ->
+      send(),
+      loop()
   end,
   loop().
+
+
+data(Data) ->
+  % remember for later use
+  todo.
+
+newTimer(WaitTime) ->
+  % start new Timer, which sends {send} to self
+  todo.
+
+send() ->
+  % send msg via multicast
+  todo.
