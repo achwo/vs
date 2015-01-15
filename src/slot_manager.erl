d29a9b3044e9a9b30db5d73a5e62a07beb8cc061
Felix Jensen <felixjensn@gmail.com>
2015-01-15 14:10:36 +0100

    fixed reserveSlot usage bug

--- src/slot_manager.erl
+++ src/slot_manager.erl
@@ -1,10 +1,9 @@
 -module(slot_manager).
 -export([start/1]).
 
--import(util, [currentTime/1, currentSlot/1, timeTillNextSlot/1, currentFrame/1,
-  timeTillTransmission/2]).
-
 -define(NUMBER_SLOTS, 25).
+-define(U, util).
+-define(L, free_slot_list).
 
 -record(s, {
   timer=nil, 
@@ -20,15 +19,12 @@ start(SyncManager) ->
 
 init(State) when State#s.sender /= nil, State#s.receiver /= nil ->
   random:seed(now()),
-  NewState = startSlotTimer(State, currentTime(State#s.sync_manager)),
-  io:format("init(bla~n", []),
-  loop(NewState#s{free_slots=free_slot_list:new(?NUMBER_SLOTS)});
+  io:format("init done~n", []),
+  loop(resetSlots(startSlotTimer(State, ?U:currentTime(State#s.sync_manager))));
 init(State) ->
   receive
-    {set_sender, SenderPID} -> 
-      init(State#s{sender=SenderPID});
-    {set_receiver, ReceiverPID} ->
-      init(State#s{receiver=ReceiverPID})
+    {set_sender, SenderPID}     -> init(State#s{sender=SenderPID});
+    {set_receiver, ReceiverPID} -> init(State#s{receiver=ReceiverPID})
   end.
 
 loop(State) ->
@@ -41,24 +37,23 @@ loop(State) ->
 
 reserveSlot(Slot, State) ->
   io:format("reserveSlot~n", []),
-  {ReservedSlot, List} = free_slot_list:reserveSlot(Slot, State#s.free_slots),
   State#s{
-    reserved_slot = ReservedSlot,
-    free_slots = List
+    reserved_slot = Slot,
+    free_slots = ?L:reserveSlot(Slot, State#s.free_slots)
   }.
 
 reserveRandomSlot(From, State) -> 
   io:format("reserveRandomSlot~n", []),
-  {Slot, List} = free_slot_list:reserveRandomSlot(State#s.free_slots),
+  {Slot, List} = ?L:reserveRandomSlot(State#s.free_slots),
   From ! {reserved_slot, Slot},
   State#s{free_slots=List}.
 
 slotEnd(State) -> 
   io:format("slotEnd~n", []),
   NewState = checkSlotInbox(State),
-  CurrentTime = currentTime(State#s.sync_manager),
+  CurrentTime = ?U:currentTime(State#s.sync_manager),
 
-  case currentSlot(CurrentTime) of
+  case ?U:currentSlot(CurrentTime) of
     1 -> NewNewState = handleFrameEnd(NewState); % todo: 0 or 1? | use result
     _ -> NewNewState = NewState
   end,
@@ -77,7 +72,7 @@ checkSlotInbox(State) ->
     {reserve_slot, SlotNumber} ->
       io:format("reserveSlot: ~p~n", [SlotNumber]),
       State#s{
-        free_slots=free_slot_list:reserveSlot(SlotNumber, State#s.free_slots)
+        free_slots=?L:reserveSlot(SlotNumber, State#s.free_slots)
       }
   end.
 
@@ -88,7 +83,7 @@ startSlotTimer(State, CurrentTime) ->
     nil -> ok;
     _ -> erlang:cancel_timer(State#s.timer)
   end,
-  WaitTime = timeTillNextSlot(CurrentTime),
+  WaitTime = ?U:timeTillNextSlot(CurrentTime),
   State#s{timer=erlang:send_after(WaitTime, self(), {slot_end})}.
 
 % Changes ReservedSlot, FreeSlotList
@@ -96,10 +91,10 @@ handleFrameEnd(State) ->
   io:format("handleFrameEnd~n", []),
   SyncManager = State#s.sync_manager,
 
-  FrameBeforeSync = currentFrame(currentTime(SyncManager)),
+  FrameBeforeSync = ?U:currentFrame(?U:currentTime(SyncManager)),
   SyncManager ! {sync},
   SyncManager ! {reset_deviations},
-  FrameAfterSync = currentFrame(currentTime(SyncManager)),
+  FrameAfterSync = ?U:currentFrame(?U:currentTime(SyncManager)),
 
   % todo: maybe this block is fucked, because my brain is right now:
   case FrameBeforeSync > FrameAfterSync of 
@@ -109,7 +104,8 @@ handleFrameEnd(State) ->
     false -> 
       {TransmissionSlot, NewState} = transmissionSlot(State), 
       TransmissionTimeOffset = 10,
-      TimeTillTransmission = TransmissionTimeOffset + timeTillTransmission(TransmissionSlot, currentTime(SyncManager)),
+      TimeTillTransmission = TransmissionTimeOffset 
+        + ?U:timeTillTransmission(TransmissionSlot, ?U:currentTime(SyncManager)),
       io:format("TimeTillTransmission: ~p~n", [TimeTillTransmission]),
       NewState#s.sender ! {new_timer, TimeTillTransmission},
       resetSlots(NewState)
@@ -118,13 +114,14 @@ handleFrameEnd(State) ->
 resetSlots(State) ->
   io:format("resetSlots~n", []),
   State#s{
-    free_slots = free_slot_list:new(?NUMBER_SLOTS),
+    free_slots = ?L:new(?NUMBER_SLOTS),
     reserved_slot = nil % todo: richtig?
   }.
 
 transmissionSlot(State) when State#s.reserved_slot == nil ->
-  {Slot, List} = free_slot_list:reserveLastFreeSlot(State#s.free_slots),
-  io:format("transmissionSlot1: currentSlot: ~p~n", [currentSlot(currentTime(State#s.sync_manager))]),
+  {Slot, List} = ?L:reserveLastFreeSlot(State#s.free_slots),
+  io:format("transmissionSlot1: currentSlot: ~p~n", 
+    [?U:currentSlot(?U:currentTime(State#s.sync_manager))]),
   io:format("transmissionSlot1: ~p~n", [Slot]),
   io:format("transmissionSlot1: ~p~n", [List]),
   {Slot, State#s{free_slots=List}};
